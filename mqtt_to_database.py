797598cdd03fab547482cc15aedb6d368ec0fcc4
Romes8 <58208649+Romes8@users.noreply.github.com>
2020-04-28 10:34:16 +0200

    Update mqtt_to_database.py

--- mqtt_to_database.py
+++ mqtt_to_database.py
@@ -4,35 +4,40 @@ import time
 import os
 import sys
 import datetime
+from time import clock
 
 while True:
-    connection = psycopg2.connect(user = "postgres",
-                                      password = "romi2000",
-                                      host = "127.0.0.1",
-                                      port = "5432",
-                                      database = "project3db")
+    connection = psycopg2.connect(user="postgres",
+                                  password="romi2000",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="project3")
     cur = connection.cursor()
 
+
     def on_connect(client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
-        client.subscribe("Romes/#")
+        print("Connected with result code " + str(rc))
+        client.subscribe("baaa/#")
+
 
     def on_message(client, userdata, msg):
-        # each room needs to have its room number (eg. Romes/projecttest/temperature/108.2)
-        #checks for temperature
-        if ("Romes/projecttest/temperature/108.2" in msg.topic): #each room needs to have its room number (eg. Romes/projecttest/temperature/108.2)
+        # each room needs to have its room number (eg. baaa/first_f/108.2/temp)
+        # checks for temperature
+        if ("baaa/first_f/108.2/temp" in msg.topic):
             global temp
             temp = float(msg.payload)
             print("Temp done")
             global done1
             done1 = 1
 
-        #checks for humidity
-        if ("Romes/projecttest/humidity/108.2" in msg.topic):
+        # checks for humidity
+        if ("baaa/first_f/108.2/hum" in msg.topic):
             global room_num
             global hum
             global timestamp
+            global floor_num
             room_num = 108.2
+            floor_num = 1
             timestamp = datetime.datetime.today()
             print(timestamp)
             hum = float(msg.payload)
@@ -40,22 +45,39 @@ while True:
             global done2
             done2 = 1
 
-        #inserting all the information to the database
-        if done1+done2 == 2:
+        if ("baaa/first_f/108.2/light" in msg.topic):
+            global light
+            light = int(msg.payload)
+            first_time = time.perf_counter()
+            print(first_time)
+            global done3
+            # done3 = 1
+            if light == 0:
+                second_time = time.perf_counter()
+                print("Timer starts")
+                third_time = time.perf_counter()
+                if third_time - second_time == 3:
+                    print("Time reached goal")
+                    timer = 1
+                else:
+                    print("Goal not met. ")
+
+        # inserting all the information to the database
+        if done1 + done2 == 2:
             print("Print z mqtt.")
             done1 = 0
             done2 = 0
-            postgres_insert_query = """ INSERT INTO testproject3 (room_id, temperature, humidity,time_stamp) VALUES (%s,%s,%s,%s)"""
-            record_to_insert = (room_num, temp, hum, timestamp)
+            postgres_insert_query = """ INSERT INTO baaa (room_id,floor_id,temperature, humidity,time_stamp) VALUES (%s,%s,%s,%s,%s)"""
+            record_to_insert = (room_num, floor_num, temp, hum, timestamp)
             cur.execute(postgres_insert_query, record_to_insert)
             print("Done")
 
-
             connection.commit()
             print("Commited")
-            #cur.close()
-            #connection.close()
-            #os.execl(sys.executable, os.path.abspath('python "C:/Users/roman/PycharmProjects/project3/mqtt_to_database.py"'), *sys.argv)
+            # cur.close()
+            # connection.close()
+            # os.execl(sys.executable, os.path.abspath('python "C:/Users/roman/PycharmProjects/project3/mqtt_to_database.py"'), *sys.argv)
+
 
     client = mqtt.Client()
     client.on_connect = on_connect
